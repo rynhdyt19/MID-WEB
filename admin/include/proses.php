29e9f77269f9439b99a1ed899f02644c7df4ec22
rynhdyt19 <104614030+rynhdyt19@users.noreply.github.com>
2023-12-23 18:15:52 +0800

    merapihkan kode proses movie

---
+++ admin/include/proses.php
@@ -0,0 +1,68 @@
+<?php
+require 'koneksi.php';
+
+function tambah_data_movie($data, $files){
+    if ($_POST['aksi'] == "tambah") {
+
+        $judul = $data['judul'];
+        $tahun = $data['tahun'];
+        $durasi = $data['durasi'];
+        $rating = $data['rating'];
+        $gambar = $files['gambar']['name'];
+    
+        $dir = "images/";
+        $tmpFile = $files['gambar']['tmp_name'];
+    
+        move_uploaded_file($tmpFile, $dir.$gambar);
+    
+        $query = "INSERT INTO movie VALUES(null, '$gambar', '$judul', '$tahun', '$durasi', '$rating');";
+        $sql = mysqli_query($GLOBALS['conn'], $query);
+
+        return true;
+    }
+}
+
+function update_data_movie($data, $files) {
+    if($data['aksi'] == 'update'){
+        $id_movie = $data['id'];
+    $judul = $data['judul'];
+    $tahun = $data['tahun'];
+    $durasi = $data['durasi'];
+    $rating = $data['rating'];
+
+    $queryShow = "SELECT * FROM movie WHERE id = '$id_movie';";
+    $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+    $result = mysqli_fetch_assoc($sqlShow);
+
+    if ($files['gambar']['name'] == "") {
+        $gambar = $result['gambar'];
+    } else {
+        $gambar = $files['gambar']['name'];
+        unlink("images/" . $result['gambar']);
+        move_uploaded_file($files['gambar']['tmp_name'], 'images/' . $files['gambar']['name']);
+    }
+
+    $query = "UPDATE movie SET gambar='$gambar', judul='$judul', tahun='$tahun', durasi='$durasi', rating='$rating' WHERE id = '$id_movie';";
+    $sql = mysqli_query($GLOBALS['conn'], $query);
+    }
+
+    return true;
+}
+
+function hapus_data_movie($ambil){
+    $id_movie = $ambil['hapus'];
+
+    $queryShow = "SELECT * FROM movie WHERE id = '$id_movie';";
+    $sqlShow = mysqli_query($GLOBALS['conn'], $queryShow);
+    $result = mysqli_fetch_assoc($sqlShow);
+
+    unlink("images/" . $result['gambar']);
+
+    $query = "DELETE FROM movie WHERE id = '$id_movie';";
+    $sql = mysqli_query($GLOBALS['conn'], $query);
+
+    return true;
+}
+
+
+?>
--- admin/proses-tambah-data-movie.php
+++ admin/proses-tambah-data-movie.php
@@ -1,73 +1,33 @@
 <?php
-require 'koneksi.php'; 
+include 'include/proses.php';
+require 'koneksi.php';
 
 if (isset($_POST['aksi'])) {
     if ($_POST['aksi'] == "tambah") {
+        $berhasil = tambah_data_movie($_POST, $_FILES);
 
-        $judul = $_POST['judul'];
-        $tahun = $_POST['tahun'];
-        $durasi = $_POST['durasi'];
-        $rating = $_POST['rating'];
-        $gambar = $_FILES['gambar']['name'];
-
-        $dir = "images/";
-        $tmpFile = $_FILES['gambar']['tmp_name'];
-
-        move_uploaded_file($tmpFile, $dir.$gambar);
-
-        $query = "INSERT INTO movie VALUES(null, '$gambar', '$judul', '$tahun', '$durasi', '$rating');";
-        $sql = mysqli_query($conn, $query);
-
-        if ($sql) {
+        if ($berhasil) {
             header('location: movie_admin.php');
         } else {
-            echo $query;
+            echo $berhasil;
         }
-    } elseif ($_POST['aksi'] == "update") {
-        $id_movie = $_POST['id'];
-        $judul = $_POST['judul'];
-        $tahun = $_POST['tahun'];
-        $durasi = $_POST['durasi'];
-        $rating = $_POST['rating'];
+    }elseif($_POST['aksi'] == "update"){
+        $berhasil = update_data_movie($_POST, $_FILES);
 
-        $queryShow = "SELECT * FROM movie WHERE id = '$id_movie';";
-        $sqlShow = mysqli_query($conn, $queryShow);
-        $result = mysqli_fetch_assoc($sqlShow);
-
-        if($_FILES['gambar']['name'] == ""){
-            $gambar = $result['gambar'];
-        }else{
-            $gambar = $_FILES['gambar']['name'];
-            unlink("images/".$result['gambar']);
-            move_uploaded_file($_FILES['gambar']['tmp_name'], 'images/'.$_FILES['gambar']['name']);
-        }
-
-        $query = "UPDATE movie SET gambar='$gambar', judul='$judul', tahun='$tahun', durasi='$durasi', rating='$rating' WHERE id = '$id_movie';";
-        $sql = mysqli_query($conn, $query);
-
-        if ($sql) {
+        if ($berhasil) {
             header('location: movie_admin.php');
         } else {
-            echo $query;
+            echo $berhasil;
         }
     }
 }
 
 if (isset($_GET['hapus'])) {
-    $id_movie = $_GET['hapus'];
+    $berhasil = hapus_data_movie($_GET);
 
-    $queryShow = "SELECT * FROM movie WHERE id = '$id_movie';";
-    $sqlShow = mysqli_query($conn, $queryShow);
-    $result = mysqli_fetch_assoc($sqlShow);
-
-    unlink("images/".$result['gambar']);
-
-    $query = "DELETE FROM movie WHERE id = '$id_movie';";
-    $sql = mysqli_query($conn, $query);
-
-    if ($sql) {
+    if ($berhasil) {
         header('location: movie_admin.php');
     } else {
-        echo $query;
+        echo $berhasil;
     }
 }
