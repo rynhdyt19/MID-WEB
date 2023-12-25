a1489c9d78a7d445db6889bbc9c71b4bbec59e6f
rynhdyt19 <104614030+rynhdyt19@users.noreply.github.com>
2023-12-26 01:44:29 +0800

    perbaiki logout

--- admin/logout.php
+++ admin/logout.php
@@ -2,16 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
-// header('Location: login.php');
-echo '<script>
-// JavaScript untuk menampilkan pesan konfirmasi sebelum logout
-window.onload = function() {
-  if (confirm("Apakah Anda yakin ingin logout?")) {
-    window.location.href = "login.php"; // Redirect ke halaman login setelah logout
-  } else {
-    window.location.href = "index.php"; // Redirect ke halaman utama jika tidak logout
-  }
-};
-</script>';
-// exit;
+header('Location: login.php');
+exit;
 ?>
