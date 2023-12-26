b11c9f35a20e8a9c54729cee9ff21d9fda1d07e0
rynhdyt19 <104614030+rynhdyt19@users.noreply.github.com>
2023-12-26 20:30:35 +0800

    ubah dikit

--- koneksi.php
+++ koneksi.php
@@ -4,7 +4,6 @@ $username = "root"; // Username MySQL
 $password = ""; // Password MySQL
 $dbname = "web_teori"; // Nama database
 
-// Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 ?>
